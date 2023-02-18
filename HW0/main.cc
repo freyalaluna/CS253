473aad77b73826afb040e2a39af47afa3ff90f42
Freya  LaLuna <98504497+freyalaluna@users.noreply.github.com>
2023-02-17 18:41:36 -0700

    Uploaded HW0 files

---
+++ HW0/main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+        // How many CU students does it take to change a light bulb?
+        cout << "Just one! I have friends at CU and I don't want to throw shade at them :(\n";
+        return 0;
+}
+
